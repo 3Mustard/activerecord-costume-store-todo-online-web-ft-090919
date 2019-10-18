cf4fcb45559b5f2b92f73cc57418cd2a98b815df
Matthew C <m23.cullen@gmail.com>
2019-10-18 15:26:56 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,9 +3,9 @@ class CreateCostumeStores < ActiveRecord::Migration
   def change 
     create_table :costume_stores do |c|
       c.string :name
-      c.integer :price
-      c.integer :size
-      c.string :image_url
+      c.integer 
+      c.integer 
+      c.string :location
       c.timestamps
     end 
   end 
