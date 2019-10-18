697351c61641f13a22642ff7a2bcf738ddfe2347
Matthew C <m23.cullen@gmail.com>
2019-10-18 15:35:28 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       c.integer :costume_inventory
       c.integer :num_of_employees
       c.boolean :still_in_business
-      c.integer :opening_time 
+      c. :opening_time 
       c.integer :closing_time
       c.timestamps
     end 
